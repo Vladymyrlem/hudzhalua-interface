a3560c543dc6d3fb6012290e34de521b0b1c2084
Vladymyr <vovangud@gmail.com>
2022-11-05 14:08:16 +0200

    Update service

--- src/UserAgentServiceInterface.php
+++ src/UserAgentServiceInterface.php
@@ -4,6 +4,8 @@ namespace Hudzhal\UserAgent;
 
 interface UserAgentServiceInterface
 {
+    public function parse($ua);
+
     public function browser(): ?string;
 
     public function os(): ?string;
